e40c6f74ce213b0691b559a7ffb9f72d28703f0f
nonsensation <3li@nonsensation.com>
2020-02-27 09:38:57 +0100

    initial commit

---
+++ NonConTroll/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NonConTroll.CodeAnalysis.Syntax;
+
+namespace NonConTroll
+{
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
+
+namespace NonConTroll.CodeAnalysis
+{
+    public class Lexer
+    {
+        private readonly string Text;
+
+        private int Position;
+        private TokenType TokenType;
+
+        public Lexer( string text )
+        {
+            this.Text = text;
+        }
+    }
+}
+
+namespace NonConTroll.CodeAnalysis.Syntax
+{
+    public enum TokenType
+    {
+
+    }
+}
